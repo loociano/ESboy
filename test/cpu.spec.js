b9a1ec6201b3a86eb31d347996538bffddb73b63
Luciano Rubio <luciano@loociano.com>
2016-08-12 20:23:14 +0100

    Break down test in two

--- test/cpu.spec.js
+++ test/cpu.spec.js
@@ -40,12 +40,18 @@ describe('CPU', function() {
     assert.equal(cpu.PC, 0x100, 'Program Counter should start at 0x100');
     assert.equal(cpu.nextCommand(), 'NOP', 'Tetris starts with NOP.');
 
-    assert.equal(cpu.AF, 0x0001, 'Register AF must start as 0x0001');
-    assert.equal(cpu.F, 0xb0, 'Register F must start as 0xb0');
-    assert.equal(cpu.BC, 0x0013, 'Register BC must start as 0x0013');
-    assert.equal(cpu.DE, 0x00d8, 'Register DE must start as 0x00d8');
-    assert.equal(cpu.HL, 0x014d, 'Register HL must start as 0x014d');
+    assert.equal(cpu.A, 0x01, 'Accumulator must start as 0x01 for GB');
+    assert.equal(cpu.AF(), 0x01, 'Register AF must start as 0x0001');
+    assert.equal(cpu.F, 0xb0, 'Flag register must start as 0xb0');
+    assert.equal(cpu.BC(), 0x0013, 'Register BC must start as 0x0013');
+    assert.equal(cpu.DE(), 0x00d8, 'Register DE must start as 0x00d8');
+    assert.equal(cpu.HL(), 0x014d, 'Register HL must start as 0x014d');
     assert.equal(cpu.SP, 0xfffe, 'Stack Pointer must start as 0xfffe');
+  });
+
+  it('should start the memory map', () => {
+
+    assert.equal(cpu.memory.length, 0x10000, 'Memory size is 0x10000');
 
     // Starting values at addresses
     assert.equal(cpu.opcodeAt(0xff05), 0x00);
@@ -79,7 +85,6 @@ describe('CPU', function() {
     assert.equal(cpu.opcodeAt(0xff4a), 0x00);
     assert.equal(cpu.opcodeAt(0xff4b), 0x00);
     assert.equal(cpu.opcodeAt(0xffff), 0x00);
-
   });
 
 });
