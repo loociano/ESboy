d04d3266bfaba62ef6229b285d3af606dc13cd34
Luciano Rubio <luciano@loociano.com>
2016-08-13 19:42:13 +0100

    Add simple Electron GUI

---
+++ src/gui.js
@@ -0,0 +1,48 @@
+import {app, BrowserWindow, Menu, remote} from 'electron';
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 256,
+    height: 256,
+    resizable: false,
+    maximizable: false
+  });
+
+  win.loadURL(`file://${__dirname}/../src/index.html`);
+
+  const template = [{
+    label: 'File',
+    submenu: [{
+        label: 'Open Game...',
+        accelerator: 'CmdOrCtrl+O',
+        role: 'open'
+      },{
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+Q',
+        role: 'close'
+      }]
+  }];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
---
+++ src/renderer.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('screen');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'black';
+ctx.fillRect(10, 10, 100, 100);
