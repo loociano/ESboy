965c7824a56811a14e9c4670a9a29c4d12a8f715
Luciano Rubio <luciano@loociano.com>
2016-08-16 23:08:13 +0100

    Add more tests for CP n

--- test/cpu.spec.js
+++ test/cpu.spec.js
@@ -295,13 +295,43 @@ describe('CPU', function() {
     assert.equal(cpu.a(), value, '(0xffff) into a');
   });
 
-  it('should compare with register a with value', () => {
+  it('should compare register a with lower value n', () => {
+    const n = 0x01;
+    cpu.ld_a_n(0xab);
+    cpu.cp_n(n);
+    assert.equal(cpu.Z(), 0, 'Z not set as a > n');
+    assert.equal(cpu.N(), 1, 'N is always set');
+    assert.equal(cpu.H(), 1, 'No half carry');
+    assert.equal(cpu.C(), 0, 'A is not greater than n');
+  });
+
+  it('should compare register a with equal value n', () => {
     const n = 0xab;
     cpu.ld_a_n(0xab);
     cpu.cp_n(n);
-    assert.equal(cpu.Z(), 0, 'Z=0 as a=n');
+    assert.equal(cpu.Z(), 1, 'Z is set as a=n');
     assert.equal(cpu.N(), 1, 'N is set');
     assert.equal(cpu.H(), 0, 'Half carry happened');
+    assert.equal(cpu.C(), 0, 'A is not greater than n');
+  });
+
+  it('should compare register a with greater value n', () => {
+    const n = 0xff;
+    cpu.ld_a_n(0xab);
+    cpu.cp_n(n);
+    assert.equal(cpu.Z(), 0, 'Z reset as a < n');
+    assert.equal(cpu.N(), 1, 'N is always set');
+    assert.equal(cpu.H(), 0, 'Half carry happened');
+    assert.equal(cpu.C(), 1, 'A is not greater than n');
+  });
+
+  it('should compare register a and test half carry', () => {
+    const n = 0x10;
+    cpu.ld_a_n(0xff);
+    cpu.cp_n(n);
+    assert.equal(cpu.Z(), 0, 'Z reset as a != n');
+    assert.equal(cpu.N(), 1, 'N is always set');
+    assert.equal(cpu.H(), 1, 'Half carry happened');
     assert.equal(cpu.C(), 0, 'A is not greater than n');
   });
 
