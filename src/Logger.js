967128ec3235a9909b58c37757f2ab206a4d20b5
Luciano Rubio <luciano@loociano.com>
2016-08-17 21:24:12 +0100

    Fix memory address

--- src/Logger.js
+++ src/Logger.js
@@ -5,7 +5,7 @@ export default class Logger {
 
   static state(cpu, fn, paramLength, param){
     if (!config.TEST) {
-      console.info(`[${Utils.hex4(cpu.pc() - paramLength)}] ${fn.name} ${Utils.hexStr(param)} af:${Utils.hex4(cpu.af())} bc:${Utils.hex4(cpu.bc())} de:${Utils.hex4(cpu.de())} hl:${Utils.hex4(cpu.hl())} sp:${Utils.hex4(cpu.sp())} pc:${Utils.hex4(cpu.pc())}`);
+      console.info(`[${Utils.hex4(cpu.pc() - paramLength - 1)}] ${fn.name} ${Utils.hexStr(param)} af:${Utils.hex4(cpu.af())} bc:${Utils.hex4(cpu.bc())} de:${Utils.hex4(cpu.de())} hl:${Utils.hex4(cpu.hl())} sp:${Utils.hex4(cpu.sp())} pc:${Utils.hex4(cpu.pc())}`);
     }
   }
 
