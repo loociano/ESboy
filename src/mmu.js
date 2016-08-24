64d2baf7cf1052c870adff917585a9c1a581f2fa
Luciano Rubio <luciano@loociano.com>
2016-08-24 23:23:57 +0100

    Implement

--- src/mmu.js
+++ src/mmu.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import Utils from './utils';
-import Logger from './logger';
 
 export default class MMU {
 
@@ -111,6 +110,17 @@ export default class MMU {
     this.memory[0xff4a] = 0x00;
     this.memory[0xff4b] = 0x00;
     this.memory[0xffff] = 0x00;
+
+    this._loadBIOS();
+  }
+
+  /**
+   * Loads BIOS in memory
+   * @private
+   */
+  _loadBIOS(){
+    const bios = new Buffer('31feffaf21ff9f32cb7c20fb2126ff0e113e8032e20c3ef3e2323e77773efce0471104012110801acd9500cd9600137bfe3420f311d80006081a1322230520f93e19ea1099212f990e0c3d2808320d20f92e0f18f3673e6457e0423e91e040041e020e0cf044fe9020fa0d20f71d20f20e13247c1e83fe6228061ec1fe6420067be20c3e87e2f04290e0421520d205204f162018cb4f0604c5cb1117c1cb11170520f522232223c9ceed6666cc0d000b03730083000c000d0008111f8889000edccc6ee6ddddd999bbbb67636e0eecccdddc999fbbb9333e3c42b9a5b9a5423c21040111a8001a13be20fe237dfe3420f506197886230520fb8620fe3e01e050', 'hex');
+    bios.copy(this.memory);
   }
 
   /**
@@ -127,6 +137,16 @@ export default class MMU {
       return this.romByteAt(addr);
     }
     return this.memory[addr];
+  }
+
+
+  /**
+   * Reads buffer from memory
+   * @param {number} addr_start, 16 bits
+   * @param {number} addr_end, 16 bits (exclusive)
+   */
+  readBuffer(addr_start, addr_end){
+    return this.memory.slice(addr_start, addr_end);
   }
 
   /**
