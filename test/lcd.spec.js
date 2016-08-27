7d48772bd5ab6cf1189ac581b291e1abfdf3a3a9
Luciano Rubio <luciano@loociano.com>
2016-08-27 13:48:36 +0100

    Write test to support levels of gray

--- test/lcd.spec.js
+++ test/lcd.spec.js
@@ -4,17 +4,52 @@ import {describe, beforeEach, it} from 'mocha';
 
 describe('LCD', () => {
 
-  it('should transform tile buffer into pixel values', () => {
-
+  it('should transform a Nintendo tile buffer into a matrix', () => {
     const array = LCD.tileToMatrix(new Buffer('3c004200b900a500b900a50042003c00', 'hex'));
-    assert.deepEqual(array, [0,0,1,1,1,1,0,0,
-                             0,1,0,0,0,0,1,0,
-                             1,0,1,1,1,0,0,1,
-                             1,0,1,0,0,1,0,1,
-                             1,0,1,1,1,0,0,1,
-                             1,0,1,0,0,1,0,1,
-                             0,1,0,0,0,0,1,0,
-                             0,0,1,1,1,1,0,0]);
+    assert.deepEqual(array, [0,0,2,2,2,2,0,0,
+                             0,2,0,0,0,0,2,0,
+                             2,0,2,2,2,0,0,2,
+                             2,0,2,0,0,2,0,2,
+                             2,0,2,2,2,0,0,2,
+                             2,0,2,0,0,2,0,2,
+                             0,2,0,0,0,0,2,0,
+                             0,0,2,2,2,2,0,0]);
+  });
+
+  it('should transform a tile buffer into levels of gray matrix', () => {
+    const array = LCD.tileToMatrix(new Buffer('3355ccaa3355ccaa3355ccaa3355ccaa', 'hex'));
+    assert.deepEqual(array, [0,1,2,3,0,1,2,3,
+                             3,2,1,0,3,2,1,0,
+                             0,1,2,3,0,1,2,3,
+                             3,2,1,0,3,2,1,0,
+                             0,1,2,3,0,1,2,3,
+                             3,2,1,0,3,2,1,0,
+                             0,1,2,3,0,1,2,3,
+                             3,2,1,0,3,2,1,0]);
+  });
+
+  it('should transform a tile buffer into a transparent matrix', () => {
+    const array = LCD.tileToMatrix(new Buffer('00000000000000000000000000000000', 'hex'));
+    assert.deepEqual(array, [0,0,0,0,0,0,0,0,
+                             0,0,0,0,0,0,0,0,
+                             0,0,0,0,0,0,0,0,
+                             0,0,0,0,0,0,0,0,
+                             0,0,0,0,0,0,0,0,
+                             0,0,0,0,0,0,0,0,
+                             0,0,0,0,0,0,0,0,
+                             0,0,0,0,0,0,0,0]);
+  });
+
+  it('should transform a tile buffer into a black matrix', () => {
+    const array = LCD.tileToMatrix(new Buffer('ffffffffffffffffffffffffffffffff', 'hex'));
+    assert.deepEqual(array, [3,3,3,3,3,3,3,3,
+                             3,3,3,3,3,3,3,3,
+                             3,3,3,3,3,3,3,3,
+                             3,3,3,3,3,3,3,3,
+                             3,3,3,3,3,3,3,3,
+                             3,3,3,3,3,3,3,3,
+                             3,3,3,3,3,3,3,3,
+                             3,3,3,3,3,3,3,3]);
   });
 
 });
