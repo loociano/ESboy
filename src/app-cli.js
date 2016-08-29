d606219523ccbdd9e789f99518dfbb7163cc4109
Luciano Rubio <luciano@loociano.com>
2016-08-29 20:48:27 +0100

    Fix import

--- src/app-cli.js
+++ src/app-cli.js
@@ -1,7 +1,10 @@
 import CPU from './cpu';
-import ContextMock from '../test/mock/contextMock';
+import ContextMock from '../lib/mock/contextMock';
 import config from './config';
 
 config.DEBUG = true;
 
+const date = new Date();
 new CPU(process.argv[2], new ContextMock()).start(0x100);
+
+console.log(`Took: ${new Date() - date} millis`);
