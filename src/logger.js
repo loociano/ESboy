677e20e05dacbea0464834c68e46161a788f2a6d
Luciano Rubio <luciano@loociano.com>
2016-09-06 22:48:23 +0100

    Use utility function to improve readability of debugging output

--- src/logger.js
+++ src/logger.js
@@ -5,7 +5,7 @@ export default class Logger {
 
   static state(cpu, fn, paramLength, param){
     if (config.DEBUG && Logger._logBIOS(cpu)) {
-      console.info(`[${Utils.hex4(cpu.pc() - paramLength - 1)}] ${fn.name} ${Utils.hexStr(param)} Z:${cpu.Z()} N:${cpu.N()} H:${cpu.H()} C:${cpu.C()}  a:${Utils.hex2(cpu.a())} bc:${Utils.hex4(cpu.bc())} de:${Utils.hex4(cpu.de())} hl:${Utils.hex4(cpu.hl())} sp:${Utils.hex4(cpu.sp())} pc:${Utils.hex4(cpu.pc())} if:${Utils.hex4(cpu.If())} ie:${Utils.hex4(cpu.ie())} ly:${cpu.mmu.ly()}`);
+      console.info(`[${Utils.hex4(cpu.pc() - paramLength - 1)}] ${Utils.str20(fn.name + ' ' + Utils.hexStr(param))} Z:${cpu.Z()} N:${cpu.N()} H:${cpu.H()} C:${cpu.C()}  a:${Utils.hex2(cpu.a())} bc:${Utils.hex4(cpu.bc())} de:${Utils.hex4(cpu.de())} hl:${Utils.hex4(cpu.hl())} sp:${Utils.hex4(cpu.sp())} pc:${Utils.hex4(cpu.pc())} if:${Utils.hex4(cpu.If())} ie:${Utils.hex4(cpu.ie())} ly:${cpu.mmu.ly()}`);
     }
   }
 
