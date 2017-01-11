379a2dcaab9808d396a989747c07234cd8e5610c
Luciano Rubio <luciano@loociano.com>
2017-01-11 09:08:32 +0000

    Use right console methods in logger

--- src/logger.js
+++ src/logger.js
@@ -3,12 +3,24 @@ import Utils from './utils';
 
 export default class Logger {
 
+  /**
+   * Logs the current instruction and state of registers
+   * @param cpu
+   * @param fn
+   * @param paramLength
+   * @param param
+   */
   static state(cpu, fn, paramLength, param){
     if (config.DEBUG && Logger._logBIOS(cpu)) {
       console.info(`[${Utils.hex4(cpu.pc() - paramLength - 1)}] ${Utils.str20(fn.name + ' ' + Utils.hexStr(param))} 0b${cpu.Z()}${cpu.N()}${cpu.H()}${cpu.C()}  a:${Utils.hex2(cpu.a())} bc:${Utils.hex4(cpu.bc())} de:${Utils.hex4(cpu.de())} hl:${Utils.hex4(cpu.hl())} sp:${Utils.hex4(cpu.sp())} pc:${Utils.hex4(cpu.pc())} if:${Utils.hex2(cpu.If())} ie:${Utils.hex2(cpu.ie())} ly:${Utils.hex2(cpu.mmu.ly())} lcdc:${Utils.hex2(cpu.lcdc())} stat:${Utils.hex2(cpu.stat())}`);
     }
   }
 
+  /**
+   * @param cpu
+   * @returns {boolean} true if should log instructions during start-up
+   * @private
+   */
   static _logBIOS(cpu){
     if (!cpu.mmu.isRunningBIOS()) {
       return true;
@@ -25,19 +37,19 @@ export default class Logger {
 
   static warn(msg){
     if (!config.TEST) {
-      console.info(`<warn> ${msg}`);
+      console.warn(`${msg}`);
     }
   }
 
   static info(msg){
     if (config.DEBUG && !config.TEST) {
-      console.info(`<info> ${msg}`);
+      console.info(`${msg}`);
     }
   }
 
   static error(msg){
     if (!config.TEST) {
-      console.error(`<error> ${msg}`);
+      console.error(`${msg}`);
     }
   }
 }
