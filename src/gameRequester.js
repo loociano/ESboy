2f01360689d2bc38dc508ef5ef4c412877f75867
Luciano Rubio <luciano@loociano.com>
2017-01-13 19:36:17 +0000

    Add simple gameRequester to perform XMLHttpRequest

---
+++ src/gameRequester.js
@@ -0,0 +1,30 @@
+export default class GameRequester {
+
+  constructor(){
+    this._games = {
+      'load-game': 'roms/load-game.gb'
+    };
+  }
+
+  /**
+   * @param {string} gameName
+   * @param {Function} callback
+   */
+  request(gameName, callback){
+
+    const file = this._games[gameName];
+
+    if (file){
+      const request = new XMLHttpRequest();
+      request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          callback.call(this, this.response);
+        }
+      };
+      request.open('GET', file, true);
+      request.responseType = 'arraybuffer';
+      request.send();
+    }
+  }
+
+}
